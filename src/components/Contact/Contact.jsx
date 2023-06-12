ad509d7523c6f46963427255fa851d25fb8d3f7a
f-rahim <freelancerrahim26@gmail.com>
2023-06-12 17:12:40 +0600

    Fix spelling mistake

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
             <div className="map-area">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387198.04724410514!2d-74.60284524235009!3d40.69633363874366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1686566930329!5m2!1sen!2sbd"
-                allowfullscreen=""
+                allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
                 className="border rounded-lg"
