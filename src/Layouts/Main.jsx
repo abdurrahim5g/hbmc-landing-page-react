b8838d0e6e3a2e763a9808ce2860b3092c19a246
f-rahim <freelancerrahim26@gmail.com>
2023-06-12 20:38:15 +0600

    Added footer

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Shared/Header/Header";
+import Footer from "../components/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 };
