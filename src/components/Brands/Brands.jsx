9858401bf1a7d1c8899f69a42213d8228af09548
f-rahim <freelancerrahim26@gmail.com>
2023-06-14 10:27:28 +0600

    Update brands logo with carousel

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,3 +1,4 @@
+import "./Brands.css";
 import Title from "../Title/Title";
 import brand1 from "../../assets/images/brands/b-1.png";
 import brand2 from "../../assets/images/brands/b-2.png";
@@ -5,9 +6,58 @@ import brand3 from "../../assets/images/brands/b-3.png";
 import brand4 from "../../assets/images/brands/b-4.png";
 import brand5 from "../../assets/images/brands/b-5.png";
 import brand6 from "../../assets/images/brands/b-6.png";
-import "./Brands.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Brands = () => {
+  const logos = [
+    brand1,
+    brand2,
+    brand3,
+    brand4,
+    brand5,
+    brand6,
+    brand1,
+    brand2,
+    brand3,
+    brand4,
+    brand5,
+    brand6,
+  ];
+
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplaySpeed: 3500,
+    speed: 3499,
+    cssEase: "linear",
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="projects-section py-12">
       <div className="container mx-auto px-4">
@@ -15,24 +65,13 @@ const Brands = () => {
 
         <div className="brands-logo mt-10">
           <div className="logos">
-            <div>
-              <img src={brand1} alt="Brand logo" className="w-auto" />
-            </div>
-            <div>
-              <img src={brand2} alt="Brand logo" className="w-auto" />
-            </div>
-            <div>
-              <img src={brand3} alt="Brand logo" className="w-auto" />
-            </div>
-            <div>
-              <img src={brand4} alt="Brand logo" className="w-auto" />
-            </div>
-            <div>
-              <img src={brand5} alt="Brand logo" className="w-auto" />
-            </div>
-            <div>
-              <img src={brand6} alt="Brand logo" className="w-auto" />
-            </div>
+            <Slider {...settings}>
+              {logos?.map((logo, index) => (
+                <div key={index}>
+                  <img src={logo} alt="Brand logo" className="w-auto" />
+                </div>
+              ))}
+            </Slider>
           </div>
         </div>
       </div>
