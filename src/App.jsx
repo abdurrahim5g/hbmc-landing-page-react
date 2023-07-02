dc58ef3dcebecd3e51eb65a0263fad69784ad2dd
f-rahim <freelancerrahim26@gmail.com>
2023-07-02 17:52:41 +0600

    Comment un_used code

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import "./RTL.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import router from "./Routers/router";
-import { useState } from "react";
-import { useEffect } from "react";
-import Placeholder from "./components/Shared/Preloader/Preloader";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import Placeholder from "./components/Shared/Preloader/Preloader";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
+  // }, []);
 
   return (
     <>
