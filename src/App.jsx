53a3bb342d3b7a87863e64422f54574582596e32
f-rahim <freelancerrahim26@gmail.com>
2023-07-02 18:42:09 +0600

    Hide placeholder by JS

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,9 @@ import "./RTL.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import router from "./Routers/router";
-// import { useState } from "react";
-// import { useEffect } from "react";
-// import Placeholder from "./components/Shared/Preloader/Preloader";
 
 function App() {
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 2000);
-  // }, []);
-
-  return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-      {/* {!loading && <Placeholder />} */}
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
