b8bc8b43aa355e91f528c6d8d730020a6fcffc14
f-rahim <freelancerrahim26@gmail.com>
2023-07-02 17:50:52 +0600

    Remove placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,25 @@ import "./RTL.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import router from "./Routers/router";
+import { useState } from "react";
+import { useEffect } from "react";
+import Placeholder from "./components/Shared/Preloader/Preloader";
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+      {/* {!loading && <Placeholder />} */}
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import LanguageProvider from "./Contex/LanguageProvider.jsx";
-import Preloader from "./components/Shared/Preloader/Preloader.jsx";
-
-window.onload = () => {
-  // console.log("Window is loades");
-};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LanguageProvider>
       <App />
-      <Preloader></Preloader>
     </LanguageProvider>
   </React.StrictMode>
 );
