52e76c726a6780da4fb9c68eb622ebd87bcd3689
f-rahim <freelancerrahim26@gmail.com>
2023-07-17 21:53:12 +0600

    Recover Projects slider again

--- src/components/LatestProject/LatestProjects.jsx
+++ src/components/LatestProject/LatestProjects.jsx
@@ -9,7 +9,55 @@ import project3 from "../../assets/images/projects/project-3.png";
 import ProjectCard from "./ProjectCard";
 import { useState } from "react";
 
+import play from "../../assets/images/play.png";
+import Slider from "react-slick";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+
 const LatestProject = () => {
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    pauseOnHover: true,
+    prevArrow: (
+      <button id="prev" className="slider-control">
+        <FaAngleLeft />
+      </button>
+    ),
+    nextArrow: (
+      <button id="next" className="slider-control">
+        <FaAngleRight />
+      </button>
+    ),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+  const projectImages = [
+    project1,
+    project2,
+    project3,
+    project1,
+    project2,
+    project3,
+  ];
+
   // Projects JSON
   const projects = [
     { image: project1, catagory: "solar" },
@@ -64,6 +112,43 @@ const LatestProject = () => {
                 active={catagory}
               ></ProjectCard>
             ))}
+          </div>
+        </div>
+
+        <div className="px-4">
+          <hr className="mt-8" />
+        </div>
+
+        <div className="projects-row mt-10">
+          <div className="projects-slider relative">
+            <Slider {...settings}>
+              {projectImages?.map((p) => (
+                <div key={p} className="px-4">
+                  <div className="single-project">
+                    <div className="project-image">
+                      <img src={p} alt="" />
+                    </div>
+
+                    <div className="project-info">
+                      <div className="play">
+                        <button>
+                          <img src={play} alt="" />
+                          <span>Watch video</span>
+                        </button>
+                      </div>
+                      <h3 className="mt-5 text-lg font-semibold">
+                        Al Raha Beach Buildings
+                      </h3>
+
+                      <p className="font-light text-sm lg:text-lg">
+                        Lorem ipsum dolor sit amet consectetur. Ac dui blandit
+                        rhoncus sit nulla sagittis.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Slider>
           </div>
         </div>
       </div>
